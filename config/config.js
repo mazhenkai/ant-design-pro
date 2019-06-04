3d81d7f458583bb7f139c7964e12d4e509b88353
mazhenkai <523395363@qq.com>
2019-06-04 16:34:54 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,6 +9,7 @@ const { pwa, primaryColor } = defaultSettings;
 // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
 const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION, TEST } = process.env;
 
+//做gis修改
 const plugins = [
   [
     'umi-plugin-react',
